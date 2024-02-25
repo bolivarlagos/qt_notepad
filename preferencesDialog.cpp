fd75081dcfdfe899e6737291d0636a8f18c445e8
bolivarlagos <bolivarlagos@gmail.com>
2024-02-25 09:46:07 -0300

    add fontTab and pluginsTab

--- preferencesDialog.cpp
+++ preferencesDialog.cpp
@@ -110,7 +110,7 @@ EditorTab::EditorTab(QWidget *parent)
 
     QCheckBox *createBackup = new QCheckBox;
     createBackup->setText("Create Backup");
-  
+
     QHBoxLayout *horizontalFile = new QHBoxLayout;
     horizontalFile->addWidget(autoSave);
     horizontalFile->addWidget(saveLine);
@@ -135,11 +135,40 @@ EditorTab::EditorTab(QWidget *parent)
 FontTab::FontTab(QWidget *parent)
     : QWidget(parent)
 {
-    QLabel *test = new QLabel;
-    test->setText("ok");
+    QCheckBox *useSystem = new QCheckBox;
+    useSystem->setText("Use the system fixed width");
+
+    QLabel *editorFont = new QLabel;
+    editorFont->setText("Editor Font: ");
+
+    QLineEdit *font = new QLineEdit;
+    font->setText("Monospace Regular 12");
+
+    QHBoxLayout *horizontalLayout = new QHBoxLayout;
+    horizontalLayout->addWidget(editorFont);
+    horizontalLayout->addWidget(font);
+
+    QVBoxLayout *verticalLayout = new QVBoxLayout;
+    verticalLayout->addLayout(horizontalLayout);
+    verticalLayout->addWidget(useSystem);
+
+    QGroupBox *fontBox = new QGroupBox;
+    fontBox->setTitle("Font");
+    fontBox->setLayout(verticalLayout);
+
+    QTextEdit *textColor = new QTextEdit;
+    textColor->setText("");
+
+    QVBoxLayout *textLayout = new QVBoxLayout;
+    textLayout->addWidget(textColor);
+
+    QGroupBox *colorScheme = new QGroupBox;
+    colorScheme->setTitle("Color Scheme");
+    colorScheme->setLayout(textLayout);
 
     QVBoxLayout *viewLayout = new QVBoxLayout;
-    viewLayout->addWidget(test);
+    viewLayout->addWidget(fontBox);
+    viewLayout->addWidget(colorScheme);
 
     setLayout(viewLayout);
 }
@@ -147,11 +176,24 @@ FontTab::FontTab(QWidget *parent)
 PluginsTab::PluginsTab(QWidget *parent)
     : QWidget(parent)
 {
-    QLabel *test = new QLabel;
-    test->setText("ok");
+    QIcon icon = QIcon::fromTheme("document-save");
+
+    QCheckBox *plugin1 = new QCheckBox;
+    plugin1->setText("Plugin 1");
+    plugin1->setIcon(icon);
+
+    QCheckBox *plugin2 = new QCheckBox;
+    plugin2->setText("Plugin 2");
+    plugin2->setIcon(icon);
+
+    QCheckBox *plugin3 = new QCheckBox;
+    plugin3->setText("Plugin 3");
+    plugin3->setIcon(icon);
 
     QVBoxLayout *viewLayout = new QVBoxLayout;
-    viewLayout->addWidget(test);
+    viewLayout->addWidget(plugin1);
+    viewLayout->addWidget(plugin2);
+    viewLayout->addWidget(plugin3);
 
     setLayout(viewLayout);
 }
